8a6dba61db052e536bb4b5864086ac18aa030448
FiX <myiphone128@mail.ru>
2025-01-07 12:32:40 +0200

    search animation fixs & header position fixed

--- js/default.js
+++ js/default.js
@@ -1,5 +1,10 @@
+function isMobile() {
+  return window.innerWidth < 576;
+}
+
 const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("search-input");
 searchBtn.addEventListener("click", () => {
+  if (!isMobile()) return;
   searchInput.classList.toggle("active");
 });
